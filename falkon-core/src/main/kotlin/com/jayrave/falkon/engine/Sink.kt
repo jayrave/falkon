c4d00efc5e87aff95ec236e1c8023a4e5a245258
jayrave <jayanthan.raveendiran@gmail.com>
2016-05-20 01:16:43 -0700

    reduce Source & Sink's API => remove byte, char & boolean related methods

--- falkon-core/src/main/kotlin/com/jayrave/falkon/engine/Sink.kt
+++ falkon-core/src/main/kotlin/com/jayrave/falkon/engine/Sink.kt
@@ -1,14 +1,11 @@
 package com.jayrave.falkon.engine
 
 interface Sink {
-    fun put(columnName: String, value: Byte)
-    fun put(columnName: String, value: Char)
     fun put(columnName: String, value: Short)
     fun put(columnName: String, value: Int)
     fun put(columnName: String, value: Long)
     fun put(columnName: String, value: Float)
     fun put(columnName: String, value: Double)
-    fun put(columnName: String, value: Boolean)
     fun put(columnName: String, value: String)
     fun put(columnName: String, value: ByteArray)
     fun putNull(columnName: String)
--- falkon-core/src/main/kotlin/com/jayrave/falkon/engine/Source.kt
+++ falkon-core/src/main/kotlin/com/jayrave/falkon/engine/Source.kt
@@ -2,14 +2,11 @@ package com.jayrave.falkon.engine
 
 interface Source {
     fun getColumnIndex(columnName: String): Int
-    fun getByte(columnIndex: Int): Byte
-    fun getChar(columnIndex: Int): Char
     fun getShort(columnIndex: Int): Short
     fun getInt(columnIndex: Int): Int
     fun getLong(columnIndex: Int): Long
     fun getFloat(columnIndex: Int): Float
     fun getDouble(columnIndex: Int): Double
-    fun getBoolean(columnIndex: Int): Boolean
     fun getString(columnIndex: Int): String
     fun getBlob(columnIndex: Int): ByteArray
     fun isNull(columnIndex: Int): Boolean
