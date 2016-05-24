20ecdf42bb454199c8e9601b7354665960e588f6
jayrave <jayanthan.raveendiran@gmail.com>
2016-05-24 01:23:22 -0700

    add TableConfigurationImpl

---
+++ falkon-core/src/main/kotlin/com/jayrave/falkon/TableConfigurationImpl.kt
@@ -0,0 +1,80 @@
+package com.jayrave.falkon
+
+import com.jayrave.falkon.engine.Engine
+import com.jayrave.falkon.engine.Sink
+import com.jayrave.falkon.exceptions.MissingConverterException
+import java.util.concurrent.ConcurrentHashMap
+
+class TableConfigurationImpl<E : Engine<S>, S : Sink>(
+        override val engine: E, override val nameFormatter: NameFormatter = CamelCaseToSnakeCaseFormatter()) :
+        TableConfiguration<E, S> {
+
+    private val convertersForNonNullTypes: MutableMap<Class<*>, Converter<*>> = ConcurrentHashMap()
+    private val convertersForNullableTypes: MutableMap<Class<*>, Converter<*>> = ConcurrentHashMap()
+
+    override fun <R> getConverterForNullableType(clazz: Class<R>): Converter<R> {
+        return getFrom(clazz, convertersForNullableTypes, true)
+    }
+
+
+    override fun <R : Any> getConverterForNonNullType(clazz: Class<R>): Converter<R> {
+        return getFrom(clazz, convertersForNonNullTypes, false)
+    }
+
+
+    /**
+     * Registers a [Converter] for the nullable form of [R] which can ge retrieved from [getConverterForNullableType]
+     * CAUTION: Overwrites converters that were previously registered for the same form of [R]
+     *
+     * @param wrapForNonNullTypeIfRequired - If `true` & if the non-null form of [R] doesn't have any registered
+     *                                       converter, the passed in [converter] will be wrapped up in a
+     *                                       [NullableToNonNullConverter] & used for non-null form of [R]
+     */
+    fun <R> registerForNullableType(clazz: Class<R>, converter: Converter<R?>, wrapForNonNullTypeIfRequired: Boolean) {
+        putIn(clazz, converter, convertersForNullableTypes)
+        if (wrapForNonNullTypeIfRequired) {
+            synchronized(convertersForNonNullTypes) {
+                if (!convertersForNonNullTypes.contains(clazz)) {
+                    putIn(clazz, NullableToNonNullConverter(converter), convertersForNonNullTypes)
+                }
+            }
+        }
+    }
+
+
+    /**
+     * Registers a [Converter] for the non-null form of [R] which can ge retrieved from [getConverterForNonNullType]
+     * CAUTION: Overwrites converters that were previously registered for the same form of [R]
+     */
+    fun <R : Any> registerForNonNullType(clazz: Class<R>, converter: Converter<R>) {
+        putIn(clazz, converter, convertersForNonNullTypes)
+    }
+
+
+
+    companion object {
+
+        private fun putIn(clazz: Class<*>, converter: Converter<*>, to: MutableMap<Class<*>, Converter<*>>) {
+            to[clazz] = converter
+        }
+
+
+        private fun <R> getFrom(clazz: Class<*>, from: Map<Class<*>, Converter<*>>, isNullableType: Boolean):
+                Converter<R> {
+
+            val converter = from[clazz]
+            @Suppress("UNCHECKED_CAST")
+            return when {
+                converter != null -> converter as Converter<R>
+                else -> {
+                    val nullability = when (isNullableType) {
+                        true -> "nullable"
+                        else -> "non-null"
+                    }
+
+                    throw MissingConverterException("Converter not found for $nullability form of $clazz")
+                }
+            }
+        }
+    }
+}
---
+++ falkon-core/src/test/kotlin/com/jayrave/falkon/TableConfigurationImplTest.kt
@@ -0,0 +1,91 @@
+package com.jayrave.falkon
+
+import com.jayrave.falkon.engine.Engine
+import com.jayrave.falkon.engine.Sink
+import com.jayrave.falkon.exceptions.MissingConverterException
+import com.nhaarman.mockito_kotlin.mock
+import org.assertj.core.api.Assertions.*
+import org.junit.Test
+import java.util.*
+
+class TableConfigurationImplTest {
+
+    val configuration = TableConfigurationImpl(mock<Engine<Sink>>(), mock())
+
+    @Test(expected = MissingConverterException::class)
+    fun testGetConverterForNullableTypeThrowsForUnregisteredType() {
+        configuration.getConverterForNullableType(UUID::class.java)
+    }
+
+
+    @Test(expected = MissingConverterException::class)
+    fun testGetConverterForNonNullTypeThrowsForUnregisteredType() {
+        configuration.getConverterForNonNullType(UUID::class.java)
+    }
+
+
+    @Test
+    fun testConverterRegistrationForNullableType() {
+        val clazz = String::class.java
+        val converter = NullableStringConverter()
+        configuration.registerForNullableType(clazz, converter, false)
+        assertThat(configuration.getConverterForNullableType(clazz)).isSameAs(converter)
+    }
+
+
+    @Test
+    fun testConverterRegistrationForNonNullType() {
+        val clazz = String::class.java
+        val converter = NullableToNonNullConverter(NullableStringConverter())
+        configuration.registerForNonNullType(clazz, converter)
+        assertThat(configuration.getConverterForNonNullType(clazz)).isSameAs(converter)
+    }
+
+
+    @Test
+    fun testConverterRegistrationForNullableTypeOverWritingExistingConverter() {
+        val clazz = String::class.java
+        val converter = NullableStringConverter()
+        configuration.registerForNullableType(clazz, converter, false)
+        assertThat(configuration.getConverterForNullableType(clazz)).isSameAs(converter)
+
+        val overWritingConverter = NullableStringConverter()
+        configuration.registerForNullableType(clazz, overWritingConverter, false)
+        assertThat(configuration.getConverterForNullableType(clazz)).isSameAs(overWritingConverter)
+    }
+
+
+    @Test
+    fun testConverterRegistrationForNonNullTypeOverWritingExistingConverter() {
+        val clazz = String::class.java
+        val converter = NullableToNonNullConverter(NullableStringConverter())
+        configuration.registerForNonNullType(clazz, converter)
+        assertThat(configuration.getConverterForNonNullType(clazz)).isSameAs(converter)
+
+        val overWritingConverter = NullableToNonNullConverter(NullableStringConverter())
+        configuration.registerForNonNullType(clazz, overWritingConverter)
+        assertThat(configuration.getConverterForNonNullType(clazz)).isSameAs(overWritingConverter)
+    }
+
+
+    @Test
+    fun testAutoWrappingNullableTypeConverterForNonNullType() {
+        val clazz = String::class.java
+        val converter = NullableStringConverter()
+        configuration.registerForNullableType(clazz, converter, true)
+        assertThat(configuration.getConverterForNullableType(clazz)).isSameAs(converter)
+        assertThat(configuration.getConverterForNonNullType(clazz)).isNotNull()
+    }
+
+
+    @Test
+    fun testAutoWrappingNullableTypeConverterDoesNotOverWriteExistingNonNullType() {
+        val clazz = String::class.java
+        val nullableConverter = NullableStringConverter()
+        val nonNullConverter = NullableToNonNullConverter(nullableConverter)
+        configuration.registerForNonNullType(clazz, nonNullConverter)
+        configuration.registerForNullableType(clazz, nullableConverter, true)
+        assertThat(configuration.getConverterForNullableType(clazz)).isSameAs(nullableConverter)
+        assertThat(configuration.getConverterForNonNullType(clazz)).isSameAs(nonNullConverter) // No over writes
+    }
+}
