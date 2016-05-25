dbf18e7b22bfd7d473f174d5f69811cbae8f5b1c
jayrave <jayanthan.raveendiran@gmail.com>
2016-05-25 01:56:17 -0700

    *) WhereBuilder is generified over AdderOrEnder for extensibility *) remove #build from WhereBuilder

--- falkon-core/src/main/kotlin/com/jayrave/falkon/dao/where/WhereBuilder.kt
+++ falkon-core/src/main/kotlin/com/jayrave/falkon/dao/where/WhereBuilder.kt
@@ -5,47 +5,44 @@ import com.jayrave.falkon.Column
 /**
  * Interface which can be used to build the WHERE clause of a SQL statement
  */
-interface WhereBuilder<T : Any, ID : Any> :
-        PredicateAdder<T, ID>,
-        CompoundConnectorAdder<T, ID> {
-
-    fun build(): Where
-}
+interface WhereBuilder<T : Any, Z : AdderOrEnder<T, Z>> :
+        PredicateAdder<T, Z>,
+        CompoundConnectorAdder<T, Z>
 
 
 /**
  * Interface that exposes methods which will add a predicate to the WHERE clause of
  * a SQL statement
  */
-interface PredicateAdder<T : Any, ID : Any> {
-    fun <C> eq(column: Column<T, C>, value: C): AdderOrEnder<T, ID>
-    fun <C> notEq(column: Column<T, C>, value: C): AdderOrEnder<T, ID>
-    fun <C> gt(column: Column<T, C>, value: C): AdderOrEnder<T, ID>
-    fun <C> ge(column: Column<T, C>, value: C): AdderOrEnder<T, ID>
-    fun <C> lt(column: Column<T, C>, value: C): AdderOrEnder<T, ID>
-    fun <C> le(column: Column<T, C>, value: C): AdderOrEnder<T, ID>
-    fun <C> between(column: Column<T, C>, low: C, high: C): AdderOrEnder<T, ID>
-    fun <C> like(column: Column<T, C>, pattern: String): AdderOrEnder<T, ID>
-    fun <C> isNull(column: Column<T, C>): AdderOrEnder<T, ID>
-    fun <C> isNotNull(column: Column<T, C>): AdderOrEnder<T, ID>
+interface PredicateAdder<T : Any, Z : AdderOrEnder<T, Z>> {
+    fun <C> eq(column: Column<T, C>, value: C): Z
+    fun <C> notEq(column: Column<T, C>, value: C): Z
+    fun <C> gt(column: Column<T, C>, value: C): Z
+    fun <C> ge(column: Column<T, C>, value: C): Z
+    fun <C> lt(column: Column<T, C>, value: C): Z
+    fun <C> le(column: Column<T, C>, value: C): Z
+    fun <C> between(column: Column<T, C>, low: C, high: C): Z
+    fun <C> like(column: Column<T, C>, pattern: String): Z
+    fun <C> isNull(column: Column<T, C>): Z
+    fun <C> isNotNull(column: Column<T, C>): Z
 }
 
 
 /**
  * Interface that exposes methods to either add AND or OR between predicates
  */
-interface SimpleConnectorAdder<T : Any, ID : Any> {
-    fun and(): AfterSimpleConnectorAdder<T, ID>
-    fun or(): AfterSimpleConnectorAdder<T, ID>
+interface SimpleConnectorAdder<T : Any, Z : AdderOrEnder<T, Z>> {
+    fun and(): AfterSimpleConnectorAdder<T, Z>
+    fun or(): AfterSimpleConnectorAdder<T, Z>
 }
 
 
 /**
  * Interface that exposes methods to group predicates and combine them
  */
-interface CompoundConnectorAdder<T : Any, ID : Any> {
-    fun and(predicate: InnerAdder<T, ID>.() -> Any?): AdderOrEnder<T, ID>
-    fun or(predicate: InnerAdder<T, ID>.() -> Any?): AdderOrEnder<T, ID>
+interface CompoundConnectorAdder<T : Any, Z : AdderOrEnder<T, Z>> {
+    fun and(predicate: InnerAdder<T>.() -> Any?): Z
+    fun or(predicate: InnerAdder<T>.() -> Any?): Z
 }
 
 
@@ -53,23 +50,23 @@ interface CompoundConnectorAdder<T : Any, ID : Any> {
  * Just a semantic extension of [SimpleConnectorAdder] that doesn't add any other method. This most
  * probably will be extended where [WhereBuilder] needs to be extended
  */
-interface AdderOrEnder<T : Any, ID : Any> : SimpleConnectorAdder<T, ID>
+interface AdderOrEnder<T : Any, Z : AdderOrEnder<T, Z>> : SimpleConnectorAdder<T, Z>
 
 
 /**
  * Just a combination of [PredicateAdder] & [CompoundConnectorAdder] which gets returned
  * by methods of [SimpleConnectorAdder] to allow adding more predicates
  */
-interface AfterSimpleConnectorAdder<T : Any, ID : Any> :
-        PredicateAdder<T, ID>,
-        CompoundConnectorAdder<T, ID>
+interface AfterSimpleConnectorAdder<T : Any, Z : AdderOrEnder<T, Z>> :
+        PredicateAdder<T, Z>,
+        CompoundConnectorAdder<T, Z>
 
 
 /**
  * The interface that will be exposed inside function objects passed to methods in [CompoundConnectorAdder]
  * to add more predicates to the WHERE clause
  */
-interface InnerAdder<T : Any, ID : Any> {
+interface InnerAdder<T : Any> {
     fun <C> eq(column: Column<T, C>, value: C)
     fun <C> notEq(column: Column<T, C>, value: C)
     fun <C> gt(column: Column<T, C>, value: C)
@@ -81,6 +78,6 @@ interface InnerAdder<T : Any, ID : Any> {
     fun <C> isNull(column: Column<T, C>)
     fun <C> isNotNull(column: Column<T, C>)
 
-    fun and(predicate: InnerAdder<T, ID>.() -> Any?)
-    fun or(predicate: InnerAdder<T, ID>.() -> Any?)
+    fun and(predicate: InnerAdder<T>.() -> Any?)
+    fun or(predicate: InnerAdder<T>.() -> Any?)
 }
