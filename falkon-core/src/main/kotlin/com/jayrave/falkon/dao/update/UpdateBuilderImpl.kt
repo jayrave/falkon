5a45b0d77baef85ce1b4f146edd603eda945576f
jayrave <jayanthan.raveendiran@gmail.com>
2016-05-26 02:04:04 -0700

    add UpdateBuilderImpl

---
+++ falkon-core/src/main/kotlin/com/jayrave/falkon/dao/update/UpdateBuilderImpl.kt
@@ -0,0 +1,69 @@
+package com.jayrave.falkon.dao.update
+
+import com.jayrave.falkon.Column
+import com.jayrave.falkon.SinkBackedDataConsumer
+import com.jayrave.falkon.Table
+import com.jayrave.falkon.dao.where.AfterSimpleConnectorAdder
+import com.jayrave.falkon.dao.where.Where
+import com.jayrave.falkon.dao.where.WhereBuilder
+import com.jayrave.falkon.dao.where.WhereBuilderImpl
+import com.jayrave.falkon.engine.Engine
+import com.jayrave.falkon.engine.Sink
+
+class UpdateBuilderImpl<T : Any, E : Engine<S>, S : Sink>(override val table: Table<T, *, E, S>) :
+        UpdateBuilder<T, E, S> {
+
+    private val dataConsumer = SinkBackedDataConsumer(table.configuration.engine.sinkFactory.create())
+    private var whereBuilder: WhereBuilderImpl<T, PredicateAdderOrEnder<T>>? = null
+
+    override fun <C> set(column: Column<T, C>, value: C): AdderOrEnder<T> {
+        return AdderOrEnderImpl().set(column, value)
+    }
+
+    private fun where(): WhereBuilder<T, PredicateAdderOrEnder<T>> {
+        whereBuilder = WhereBuilderImpl { PredicateAdderOrEnderImpl(it) }
+        return whereBuilder!!
+    }
+
+    private fun update(): Int {
+        val where: Where? = whereBuilder?.build()
+        return table.configuration.engine.update(table.name, dataConsumer.sink, where?.clause, where?.arguments)
+    }
+
+
+    private inner class AdderOrEnderImpl : AdderOrEnder<T> {
+
+        override fun <C> set(column: Column<T, C>, value: C): AdderOrEnder<T> {
+            dataConsumer.setColumnName(column.name)
+            column.putStorageFormIn(value, dataConsumer)
+            return this
+        }
+
+        override fun where(): WhereBuilder<T, PredicateAdderOrEnder<T>> {
+            return this@UpdateBuilderImpl.where()
+        }
+
+        override fun update(): Int {
+            return this@UpdateBuilderImpl.update()
+        }
+    }
+
+
+    private inner class PredicateAdderOrEnderImpl(private val delegate: WhereBuilderImpl<T, PredicateAdderOrEnder<T>>) :
+            PredicateAdderOrEnder<T> {
+
+        override fun and(): AfterSimpleConnectorAdder<T, PredicateAdderOrEnder<T>> {
+            delegate.and()
+            return delegate
+        }
+
+        override fun or(): AfterSimpleConnectorAdder<T, PredicateAdderOrEnder<T>> {
+            delegate.or()
+            return delegate
+        }
+
+        override fun update(): Int {
+            return this@UpdateBuilderImpl.update()
+        }
+    }
+}
---
+++ falkon-core/src/test/kotlin/com/jayrave/falkon/dao/update/UpdateBuilderImplTest.kt
@@ -0,0 +1,130 @@
+package com.jayrave.falkon.dao.update
+
+import com.jayrave.falkon.*
+import com.jayrave.falkon.engine.Engine
+import com.jayrave.falkon.engine.Factory
+import com.jayrave.falkon.engine.Sink
+import com.jayrave.falkon.lib.MapBackedSink
+import com.nhaarman.mockito_kotlin.*
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import java.util.*
+
+class UpdateBuilderImplTest {
+
+    private val table = TableForTest()
+    private val engine = table.configuration.engine
+    private val builder = UpdateBuilderImpl(table)
+
+    @Test
+    fun testUpdateWithoutWhere() {
+        val inputValue = 5
+        val expectedSunkValues = HashMap<String, Any?>()
+        expectedSunkValues[table.int.name] = table.int.computeStorageFormOf(inputValue)
+
+        builder.set(table.int, inputValue).update()
+        verifyCallToUpdate(expectedSunkValues, null, null)
+    }
+
+
+    @Test
+    fun testUpdateWithWhere() {
+        val inputValue = 5
+        val expectedSunkValues = HashMap<String, Any?>()
+        expectedSunkValues[table.int.name] = table.int.computeStorageFormOf(inputValue)
+
+        builder.set(table.int, inputValue).where().eq(table.string, "test").update()
+        verifyCallToUpdate(expectedSunkValues, "string = ?", listOf("test"))
+    }
+
+
+    @Test
+    fun testCanUpdateMultipleColumns() {
+        val inputInt = 5
+        val inputString = "test"
+        val expectedSunkValues = HashMap<String, Any?>()
+        expectedSunkValues[table.int.name] = table.int.computeStorageFormOf(inputInt)
+        expectedSunkValues[table.string.name] = table.string.computeStorageFormOf(inputString)
+
+        builder.set(table.int, inputInt).set(table.string, inputString).update()
+        verifyCallToUpdate(expectedSunkValues, null, null)
+    }
+
+
+    @Test
+    fun testSetOverwritesExistingValueForTheSameColumn() {
+        val initialValue = 5
+        val overwritingValue = initialValue + 1
+        val expectedSunkValues = HashMap<String, Any?>()
+        expectedSunkValues[table.int.name] = table.int.computeStorageFormOf(overwritingValue)
+
+        builder.set(table.int, initialValue).set(table.int, overwritingValue).update()
+        verifyCallToUpdate(expectedSunkValues, null, null)
+    }
+
+
+    @Test
+    fun testDefiningSetAndWhereClausesDoesNotFireAnUpdateCall() {
+        builder.set(table.int, 5).where().eq(table.string, "test")
+        verify(engine, never()).update(any(), any(), any(), anyCollection())
+    }
+
+
+    @Test
+    fun testUpdateReportsCorrectRowCount() {
+        val numberOfRowsAffected = 183
+        whenever(engine.update(any(), any(), any(), anyCollection())).thenReturn(numberOfRowsAffected)
+        assertThat(builder.set(table.int, 5).where().eq(table.string, "test").update()).isEqualTo(numberOfRowsAffected)
+    }
+
+
+    private fun verifyCallToUpdate(
+            expectedSunkValues: Map<String, Any?>, expectedWhereClause: String?,
+            expectedWhereArgs: Iterable<Any?>?) {
+
+        // TableConfigurationForTest uses an mock engine that returns a factory that generates MapBackedSink.
+        // Capture and compare it against the passed in expectedSunkValues
+        val sinkCaptor = argumentCaptor<Sink>()
+        verify(engine).update(
+                eq(table.name), capture(sinkCaptor),
+                if (expectedWhereClause != null) eq(expectedWhereClause) else isNull<String>(),
+                if (expectedWhereArgs != null) eq(expectedWhereArgs) else isNull<Iterable<*>>()
+        )
+
+        val actualSunkValues = (sinkCaptor.value as MapBackedSink).sunkValues()
+        assertThat(actualSunkValues).isEqualTo(expectedSunkValues)
+    }
+
+
+
+    private class ModelForTest(
+            val int: Int = 0,
+            val string: String = "test"
+    )
+
+
+
+    private class TableForTest(
+            configuration: TableConfiguration<Engine<Sink>, Sink> = defaultConfiguration()) :
+            BaseTable<ModelForTest, Int, Engine<Sink>, Sink>("test", configuration) {
+
+        override val idColumn: Column<ModelForTest, Int> = mock()
+        override fun create(value: Value<ModelForTest>) = throw UnsupportedOperationException()
+
+        val int = col(ModelForTest::int)
+        val string = col(ModelForTest::string)
+
+        companion object {
+            private fun defaultConfiguration(): TableConfiguration<Engine<Sink>, Sink> {
+                val engineMock: Engine<Sink> = mock()
+                whenever(engineMock.sinkFactory).thenReturn(object : Factory<Sink> {
+                    override fun create() = MapBackedSink()
+                })
+
+                val configuration = TableConfigurationImpl(engineMock)
+                configuration.registerDefaultConverters()
+                return configuration
+            }
+        }
+    }
+}
