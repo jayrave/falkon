fb5b5d33172f8756def4c21dd32046df34ddac16
jayrave <jayanthan.raveendiran@gmail.com>
2016-05-26 02:03:41 -0700

    add UpdateBuilder

---
+++ falkon-core/src/main/kotlin/com/jayrave/falkon/dao/update/UpdateBuilder.kt
@@ -0,0 +1,45 @@
+package com.jayrave.falkon.dao.update
+
+import com.jayrave.falkon.Column
+import com.jayrave.falkon.Table
+import com.jayrave.falkon.dao.where.WhereBuilder
+import com.jayrave.falkon.engine.Engine
+import com.jayrave.falkon.engine.Sink
+
+interface UpdateBuilder<T : Any, E: Engine<S>, S : Sink> {
+
+    val table: Table<T, *, E, S>
+
+    /**
+     * Calling this method again for a column that has already been set should overwrite the existing value
+     */
+    fun <C> set(column: Column<T, C>, value: C): AdderOrEnder<T>
+}
+
+
+interface AdderOrEnder<T : Any> {
+
+    /**
+     * @see [UpdateBuilder.set]
+     */
+    fun <C> set(column: Column<T, C>, value: C): AdderOrEnder<T>
+
+    /**
+     * Use to build the WHERE clause of UPDATE SQL statement. Each call would erase the previously configured
+     * WHERE clause and start creating a new one
+     */
+    fun where(): WhereBuilder<T, PredicateAdderOrEnder<T>>
+
+    /**
+     * @return - Number of rows affected by this update operation
+     */
+    fun update(): Int
+}
+
+
+/**
+ * To access some [AdderOrEnder] methods conveniently after chaining calls on [WhereBuilder]
+ */
+interface PredicateAdderOrEnder<T : Any> : com.jayrave.falkon.dao.where.AdderOrEnder<T, PredicateAdderOrEnder<T>> {
+    fun update(): Int
+}
