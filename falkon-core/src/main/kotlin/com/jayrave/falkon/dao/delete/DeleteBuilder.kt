5acbea24131f0153879c0b883cc6da7d4fc4a5d2
jayrave <jayanthan.raveendiran@gmail.com>
2016-05-25 23:16:46 -0700

    add DeleteBuilder

---
+++ falkon-core/src/main/kotlin/com/jayrave/falkon/dao/delete/DeleteBuilder.kt
@@ -0,0 +1,28 @@
+package com.jayrave.falkon.dao.delete
+
+import com.jayrave.falkon.Table
+import com.jayrave.falkon.dao.where.WhereBuilder
+
+interface DeleteBuilder<T : Any> {
+
+    val table: Table<T, *, *, *>
+
+    /**
+     * Use to build the WHERE clause of DELETE SQL statement. Each call would erase the previously configured
+     * WHERE clause and start creating a new one
+     */
+    fun where(): WhereBuilder<T, AdderOrEnder<T>>
+
+    /**
+     * @return - Number of rows affected by this delete operation
+     */
+    fun delete(): Int
+}
+
+
+/**
+ * To access some [DeleteBuilder] methods conveniently after chaining calls on [WhereBuilder]
+ */
+interface AdderOrEnder<T : Any> : com.jayrave.falkon.dao.where.AdderOrEnder<T, AdderOrEnder<T>> {
+    fun delete(): Int
+}
