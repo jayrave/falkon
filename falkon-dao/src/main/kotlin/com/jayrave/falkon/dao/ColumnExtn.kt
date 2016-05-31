b6df6d60168e2ac88db4239ad0e43046283c3d34
jayrave <jayanthan.raveendiran@gmail.com>
2016-05-30 22:00:51 -0400

    add ColumnExtn

---
+++ falkon-dao/src/main/kotlin/com/jayrave/falkon/dao/ColumnExtn.kt
@@ -0,0 +1,10 @@
+package com.jayrave.falkon.dao
+
+import com.jayrave.falkon.Column
+
+/**
+ * Extracts the property for the column from the passed in instance ([t])
+ */
+internal fun <T : Any, C> Column<T, C>.extractPropertyFrom(t: T): C {
+    return propertyExtractor.extract(t)
+}
---
+++ falkon-dao/src/test/kotlin/com/jayrave/falkon/dao/ColumnExtnTest.kt
@@ -0,0 +1,15 @@
+package com.jayrave.falkon.dao
+
+import org.assertj.core.api.Assertions.*
+import org.junit.Test
+
+class ColumnExtnTest {
+
+    @Test
+    fun testExtractPropertyFrom() {
+        val inputValue = "this could be anything"
+        val model = ModelForTest(string = inputValue)
+        val actualValue = TableForTest().string.extractPropertyFrom(model)
+        assertThat(actualValue).isSameAs(inputValue)
+    }
+}
