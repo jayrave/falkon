648ce76814a84fb98ccc1b78cbdcb44de3aa0eb2
jayrave <jayanthan.raveendiran@gmail.com>
2016-06-01 01:20:21 -0400

    add robolectric to android-sqlite module

---
+++ falkon-android-sqlite/src/test/kotlin/com/jayrave/falkon/android/sqlite/RobolectricTestBaseClass.kt
@@ -0,0 +1,10 @@
+package com.jayrave.falkon.android.sqlite
+
+import android.os.Build
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricGradleTestRunner
+import org.robolectric.annotation.Config
+
+@Config(constants = BuildConfig::class, sdk = intArrayOf(Build.VERSION_CODES.LOLLIPOP))
+@RunWith(RobolectricGradleTestRunner::class)
+abstract class RobolectricTestBaseClass
