8cae36de4b91decb2b59e3ee1c030d0f2300d8ca
jayrave <jayanthan.raveendiran@gmail.com>
2016-06-13 10:19:53 -0700

    add ResultSetBackedSource

---
+++ falkon-jdbc-h2/src/main/kotlin/com/jayrave/falkon/jdbc/h2/ResultSetBackedSource.kt
@@ -0,0 +1,34 @@
+package com.jayrave.falkon.jdbc.h2
+
+import com.jayrave.falkon.engine.Source
+import java.sql.ResultSet
+
+class ResultSetBackedSource(private val resultSet: ResultSet) : Source {
+
+    override val position: Int
+        get() = resultSet.row
+
+    override fun move(offset: Int): Boolean = resultSet.relative(offset)
+    override fun moveToPosition(position: Int): Boolean = resultSet.absolute(position)
+    override fun moveToFirst(): Boolean = resultSet.first()
+    override fun moveToLast(): Boolean = resultSet.last()
+    override fun moveToNext(): Boolean = resultSet.next()
+    override fun moveToPrevious(): Boolean = resultSet.previous()
+    override fun getColumnIndex(columnName: String): Int = resultSet.findColumn(columnName)
+    override fun getShort(columnIndex: Int): Short = resultSet.getShort(columnIndex)
+    override fun getInt(columnIndex: Int): Int = resultSet.getInt(columnIndex)
+    override fun getLong(columnIndex: Int): Long = resultSet.getLong(columnIndex)
+    override fun getFloat(columnIndex: Int): Float = resultSet.getFloat(columnIndex)
+    override fun getDouble(columnIndex: Int): Double = resultSet.getDouble(columnIndex)
+    override fun getString(columnIndex: Int): String = resultSet.getString(columnIndex)
+
+    override fun getBlob(columnIndex: Int): ByteArray {
+        val blob = resultSet.getBlob(columnIndex)
+        return blob.getBytes(1, blob.length().toInt())
+    }
+
+    override fun isNull(columnIndex: Int): Boolean {
+        resultSet.getObject(columnIndex)
+        return resultSet.wasNull()
+    }
+}
---
+++ falkon-jdbc-h2/src/test/kotlin/com/jayrave/falkon/jdbc/h2/ResultSetBackedSourceTest.kt
@@ -0,0 +1,203 @@
+package com.jayrave.falkon.jdbc.h2
+
+import com.nhaarman.mockito_kotlin.*
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import java.sql.Blob
+import java.sql.ResultSet
+
+/**
+ * Just to make sure that the calls are forwarded to appropriate methods in [ResultSet] and
+ * return values are appropriately returned
+ */
+class ResultSetBackedSourceTest {
+
+    private val resultSetMock: ResultSet = mock()
+    private val source = ResultSetBackedSource(resultSetMock)
+
+    @Test
+    fun testGetPosition() {
+        whenever(resultSetMock.row).thenReturn(5)
+        val actualPosition = source.position
+
+        assertThat(actualPosition).isEqualTo(5)
+        verify(resultSetMock).row
+        verifyNoMoreInteractions(resultSetMock)
+    }
+
+    @Test
+    fun testMove() {
+        whenever(resultSetMock.relative(eq(5))).thenReturn(true).thenReturn(false)
+        val validMoveReturnValue = source.move(5)
+        val invalidMoveReturnValue = source.move(5)
+
+        assertThat(validMoveReturnValue).isTrue()
+        assertThat(invalidMoveReturnValue).isFalse()
+
+        verify(resultSetMock, times(2)).relative(eq(5))
+        verifyNoMoreInteractions(resultSetMock)
+    }
+
+    @Test
+    fun testMoveToPosition() {
+        whenever(resultSetMock.absolute(eq(5))).thenReturn(true).thenReturn(false)
+        val validMoveReturnValue = source.moveToPosition(5)
+        val invalidMoveReturnValue = source.moveToPosition(5)
+
+        assertThat(validMoveReturnValue).isTrue()
+        assertThat(invalidMoveReturnValue).isFalse()
+
+        verify(resultSetMock, times(2)).absolute(eq(5))
+        verifyNoMoreInteractions(resultSetMock)
+    }
+
+    @Test
+    fun testMoveToFirst() {
+        whenever(resultSetMock.first()).thenReturn(true).thenReturn(false)
+        val validMoveReturnValue = source.moveToFirst()
+        val invalidMoveReturnValue = source.moveToFirst()
+
+        assertThat(validMoveReturnValue).isTrue()
+        assertThat(invalidMoveReturnValue).isFalse()
+
+        verify(resultSetMock, times(2)).first()
+        verifyNoMoreInteractions(resultSetMock)
+    }
+
+    @Test
+    fun testMoveToLast() {
+        whenever(resultSetMock.last()).thenReturn(true).thenReturn(false)
+        val validMoveReturnValue = source.moveToLast()
+        val invalidMoveReturnValue = source.moveToLast()
+
+        assertThat(validMoveReturnValue).isTrue()
+        assertThat(invalidMoveReturnValue).isFalse()
+
+        verify(resultSetMock, times(2)).last()
+        verifyNoMoreInteractions(resultSetMock)
+    }
+
+    @Test
+    fun testMoveToNext() {
+        whenever(resultSetMock.next()).thenReturn(true).thenReturn(false)
+        val validMoveReturnValue = source.moveToNext()
+        val invalidMoveReturnValue = source.moveToNext()
+
+        assertThat(validMoveReturnValue).isTrue()
+        assertThat(invalidMoveReturnValue).isFalse()
+
+        verify(resultSetMock, times(2)).next()
+        verifyNoMoreInteractions(resultSetMock)
+    }
+
+    @Test
+    fun testMoveToPrevious() {
+        whenever(resultSetMock.previous()).thenReturn(true).thenReturn(false)
+        val validMoveReturnValue = source.moveToPrevious()
+        val invalidMoveReturnValue = source.moveToPrevious()
+
+        assertThat(validMoveReturnValue).isTrue()
+        assertThat(invalidMoveReturnValue).isFalse()
+
+        verify(resultSetMock, times(2)).previous()
+        verifyNoMoreInteractions(resultSetMock)
+    }
+
+    @Test
+    fun testGetColumnIndex() {
+        whenever(resultSetMock.findColumn("test")).thenReturn(5)
+        val actualColumnIndex = source.getColumnIndex("test")
+
+        assertThat(actualColumnIndex).isEqualTo(5)
+        verify(resultSetMock).findColumn(eq("test"))
+        verifyNoMoreInteractions(resultSetMock)
+    }
+
+    @Test
+    fun testGetShort() {
+        whenever(resultSetMock.getShort(5)).thenReturn(6)
+        val actualValue = source.getShort(5)
+
+        assertThat(actualValue).isEqualTo(6)
+        verify(resultSetMock).getShort(eq(5))
+        verifyNoMoreInteractions(resultSetMock)
+    }
+
+    @Test
+    fun testGetInt() {
+        whenever(resultSetMock.getInt(5)).thenReturn(6)
+        val actualValue = source.getInt(5)
+
+        assertThat(actualValue).isEqualTo(6)
+        verify(resultSetMock).getInt(eq(5))
+        verifyNoMoreInteractions(resultSetMock)
+    }
+
+    @Test
+    fun testGetLong() {
+        whenever(resultSetMock.getLong(5)).thenReturn(6)
+        val actualValue = source.getLong(5)
+
+        assertThat(actualValue).isEqualTo(6)
+        verify(resultSetMock).getLong(eq(5))
+        verifyNoMoreInteractions(resultSetMock)
+    }
+
+    @Test
+    fun testGetFloat() {
+        whenever(resultSetMock.getFloat(5)).thenReturn(6F)
+        val actualValue = source.getFloat(5)
+
+        assertThat(actualValue).isEqualTo(6F)
+        verify(resultSetMock).getFloat(eq(5))
+        verifyNoMoreInteractions(resultSetMock)
+    }
+
+    @Test
+    fun testGetDouble() {
+        whenever(resultSetMock.getDouble(5)).thenReturn(6.0)
+        val actualValue = source.getDouble(5)
+
+        assertThat(actualValue).isEqualTo(6.0)
+        verify(resultSetMock).getDouble(eq(5))
+        verifyNoMoreInteractions(resultSetMock)
+    }
+
+    @Test
+    fun testGetString() {
+        whenever(resultSetMock.getString(5)).thenReturn("test")
+        val actualValue = source.getString(5)
+
+        assertThat(actualValue).isEqualTo("test")
+        verify(resultSetMock).getString(eq(5))
+        verifyNoMoreInteractions(resultSetMock)
+    }
+
+    @Test
+    fun testGetBlob() {
+        val blobMock: Blob = mock()
+        val inputByteArray = byteArrayOf(5)
+        whenever(blobMock.length()).thenReturn(5)
+        whenever(blobMock.getBytes(1, 5)).thenReturn(inputByteArray)
+        whenever(resultSetMock.getBlob(5)).thenReturn(blobMock)
+        val actualValue = source.getBlob(5)
+
+        assertThat(actualValue).isEqualTo(inputByteArray)
+        verify(resultSetMock).getBlob(eq(5))
+        verifyNoMoreInteractions(resultSetMock)
+    }
+
+    @Test
+    fun testIsNull() {
+        whenever(resultSetMock.wasNull()).thenReturn(true).thenReturn(false)
+        val validMoveReturnValue = source.isNull(5)
+        val invalidMoveReturnValue = source.isNull(5)
+
+        assertThat(validMoveReturnValue).isTrue()
+        assertThat(invalidMoveReturnValue).isFalse()
+
+        verify(resultSetMock, times(2)).getObject(5)
+        verify(resultSetMock, times(2)).wasNull()
+        verifyNoMoreInteractions(resultSetMock)
+    }
+}
