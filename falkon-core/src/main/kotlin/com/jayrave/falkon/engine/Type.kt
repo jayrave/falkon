5e9bac6926cfcef76d24c478242d113faa3800af
jayrave <jayanthan.raveendiran@gmail.com>
2016-06-15 00:24:18 -0700

    add Type (an enum class representing all types that should be natively supported by all engines)

---
+++ falkon-core/src/main/kotlin/com/jayrave/falkon/engine/Type.kt
@@ -0,0 +1,16 @@
+package com.jayrave.falkon.engine
+
+/**
+ * Represents all types natively supported by the engine. All engines can handle
+ * `null` in addition to those listed here (usually the type null is associated
+ * is required => use [TypedNull] for this purpose)
+ */
+enum class Type {
+    SHORT,
+    INT,
+    LONG,
+    FLOAT,
+    DOUBLE,
+    STRING,
+    BLOB
+}
