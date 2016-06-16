fc9f76fc28c93d9043ecbc501a934937a88e97f3
jayrave <jayanthan.raveendiran@gmail.com>
2016-06-16 01:18:14 -0700

    add TransactionManager interface

---
+++ falkon-engine-jdbc/src/main/kotlin/com/jayrave/falkon/jdbc/h2/TransactionManager.kt
@@ -0,0 +1,25 @@
+package com.jayrave.falkon.jdbc.h2
+
+import java.sql.Connection
+
+/**
+ * All implementations must also support nested transactions
+ */
+internal interface TransactionManager {
+
+    /**
+     * The passed in [operation] will be executed inside a transaction
+     */
+    fun <R> executeInTransaction(operation: () -> R): R?
+
+    /**
+     * Any [Connection] acquired via this method shouldn't be closed explicitly. When the
+     * transaction gets over, connection will be closed automatically
+     */
+    fun getConnectionIfInTransaction(): Connection?
+
+    /**
+     * @return whether the passed in [connection] belongs in the transaction (if there is one)
+     */
+    fun belongsToTransaction(connection: Connection): Boolean
+}
