f0b61d15c56298c6233f436828df30d5ebd10ebf
jayrave <jayanthan.raveendiran@gmail.com>
2016-06-27 10:09:47 -0700

    add CompiledStatementKt in `falkon-dao`

---
+++ falkon-dao/src/main/kotlin/com/jayrave/falkon/dao/CompiledStatementKt.kt
@@ -0,0 +1,17 @@
+package com.jayrave.falkon.dao
+
+import com.jayrave.falkon.Column
+import com.jayrave.falkon.engine.CompiledStatement
+import com.jayrave.falkon.engine.TypedNull
+import com.jayrave.falkon.engine.bind
+
+/**
+ * [column] of [item] will be bound using [bind]. If the column value is null, appropriate
+ * [TypedNull] is used
+ */
+fun <T: Any, CS: CompiledStatement<R>, R> CS.bindColumn(
+        index: Int, column: Column<T, *>, item: T): CS {
+
+    bind(index + 1, column.extractPropertyFrom(item) ?: TypedNull(column.dbType))
+    return this
+}
