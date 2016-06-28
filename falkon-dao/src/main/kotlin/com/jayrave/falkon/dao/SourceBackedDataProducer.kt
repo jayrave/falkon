9fd40bef9f994891dfede13c824c6a8c112b7e8f
jayrave <jayanthan.raveendiran@gmail.com>
2016-06-27 22:13:38 -0700

    make SourceBackedDataProducer useful by making it a little less stricter

--- falkon-dao/src/main/kotlin/com/jayrave/falkon/dao/SourceBackedDataProducer.kt
+++ falkon-dao/src/main/kotlin/com/jayrave/falkon/dao/SourceBackedDataProducer.kt
@@ -5,37 +5,31 @@ import com.jayrave.falkon.engine.Source
 import com.jayrave.falkon.exceptions.DataProducerException
 
 /**
- * A [DataProducer] that gets the required value from the underlying [Source]. Failing to call
- * [setColumnIndex] before every #get/is*() call will result in [DataProducerException]
+ * A [DataProducer] that gets the required value from the underlying [Source]. Update the
+ * column index of [Source] to talk with as & when required via [setColumnIndex]
  */
 internal class SourceBackedDataProducer(val source: Source) : DataProducer {
 
     private var columnIndex: Int = INVALID_COLUMN_INDEX
 
-    override fun getBlob(): ByteArray?  = source.getBlob(getAndInvalidateColumnIndex())
-    override fun getDouble(): Double?  = source.getDouble(getAndInvalidateColumnIndex())
-    override fun getFloat(): Float?  = source.getFloat(getAndInvalidateColumnIndex())
-    override fun getInt(): Int?  = source.getInt(getAndInvalidateColumnIndex())
-    override fun getLong(): Long?  = source.getLong(getAndInvalidateColumnIndex())
-    override fun getShort(): Short?  = source.getShort(getAndInvalidateColumnIndex())
-    override fun getString(): String?  = source.getString(getAndInvalidateColumnIndex())
-    override fun isNull(): Boolean  = source.isNull(getAndInvalidateColumnIndex())
+    override fun getBlob(): ByteArray?  = source.getBlob(getValidColumnIndex())
+    override fun getDouble(): Double?  = source.getDouble(getValidColumnIndex())
+    override fun getFloat(): Float?  = source.getFloat(getValidColumnIndex())
+    override fun getInt(): Int?  = source.getInt(getValidColumnIndex())
+    override fun getLong(): Long?  = source.getLong(getValidColumnIndex())
+    override fun getShort(): Short?  = source.getShort(getValidColumnIndex())
+    override fun getString(): String?  = source.getString(getValidColumnIndex())
+    override fun isNull(): Boolean  = source.isNull(getValidColumnIndex())
 
     fun setColumnIndex(columnIndex: Int) {
         this.columnIndex = columnIndex
     }
 
-    private fun getAndInvalidateColumnIndex(): Int {
-        val result = when (columnIndex) {
-            INVALID_COLUMN_INDEX -> throw DataProducerException(
-                    "Calling #get without setting a column index"
-            )
-
-            else -> columnIndex
+    private fun getValidColumnIndex(): Int {
+        return when {
+            columnIndex != INVALID_COLUMN_INDEX -> columnIndex
+            else -> throw DataProducerException("Calling #get without setting a column index")
         }
-
-        columnIndex = INVALID_COLUMN_INDEX
-        return result
     }
 
 
