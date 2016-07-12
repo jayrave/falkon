942e6a3c894627508b8bd6d673afeefe7f25632a
jayrave <jayanthan.raveendiran@gmail.com>
2016-07-12 02:12:51 -0700

    move implementations from `falkon-mapper` to `falkon-mapper-default`

--- falkon-mapper-default/src/main/kotlin/com/jayrave/falkon/mapper/SimplePropertyExtractor.kt
+++ falkon-mapper-default/src/main/kotlin/com/jayrave/falkon/mapper/SimplePropertyExtractor.kt
@@ -5,7 +5,7 @@ import kotlin.reflect.KProperty1
 /**
  * A [PropertyExtractor] that extracts the value using the property's getter
  */
-class SimplePropertyExtractor<T : Any, C>(private val property: KProperty1<T, C>) :
+class SimplePropertyExtractor<in T : Any, out C>(private val property: KProperty1<T, C>) :
         PropertyExtractor<T, C> {
 
     override fun extract(t: T) = property.get(t)
