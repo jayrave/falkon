5cba59f9238bdde070bbd27ac1b7029f55ad2389
jayrave <jayanthan.raveendiran@gmail.com>
2016-07-17 02:20:35 -0700

    refactor BaseTable

--- falkon-mapper-basic/src/main/kotlin/com/jayrave/falkon/mapper/BaseTable.kt
+++ falkon-mapper-basic/src/main/kotlin/com/jayrave/falkon/mapper/BaseTable.kt
@@ -1,8 +1,11 @@
 package com.jayrave.falkon.mapper
 
+import com.jayrave.falkon.mapper.TableImplementationHelper.buildDefaultExtractorFrom
+import com.jayrave.falkon.mapper.TableImplementationHelper.getDefaultNullFromSqlSubstitute
+import com.jayrave.falkon.mapper.TableImplementationHelper.getDefaultNullToSqlSubstitute
+import com.jayrave.falkon.mapper.TableImplementationHelper.getJavaClassFor
 import java.util.*
 import java.util.concurrent.ConcurrentHashMap
-import kotlin.reflect.KClass
 import kotlin.reflect.KProperty1
 
 /**
@@ -21,10 +24,10 @@ abstract class BaseTable<T : Any, ID : Any>(
     inline fun <reified C> col(
             property: KProperty1<T, C>,
             name: String = computeFormattedNameOf(property),
-            converter: Converter<C> = configuration.getConverterForNullableType(getJavaClassFor(C::class)),
-            nullFromSqlSubstitute: NullSubstitute<C> = buildNullFromSqlSubstitute(true),
-            nullToSqlSubstitute: NullSubstitute<C> = buildNullToSqlSubstitute(true),
-            propertyExtractor: PropertyExtractor<T, C> = buildPropertyExtractorFrom(property)):
+            converter: Converter<C> = getConverterForNullableType(),
+            nullFromSqlSubstitute: NullSubstitute<C> = getDefaultNullFromSqlSubstitute(true),
+            nullToSqlSubstitute: NullSubstitute<C> = getDefaultNullToSqlSubstitute(true),
+            propertyExtractor: PropertyExtractor<T, C> = buildDefaultExtractorFrom(property)):
             Column<T, C> {
 
         return addColumn(
@@ -36,25 +39,29 @@ abstract class BaseTable<T : Any, ID : Any>(
     inline fun <reified C : Any> col(
             property: KProperty1<T, C>,
             name: String = computeFormattedNameOf(property),
-            converter: Converter<C> = configuration.getConverterForNonNullType(getJavaClassFor(C::class)),
-            nullFromSqlSubstitute: NullSubstitute<C> = buildNullFromSqlSubstitute(false),
-            propertyExtractor: PropertyExtractor<T, C> = buildPropertyExtractorFrom(property)):
+            converter: Converter<C> = getConverterForNonNullType(),
+            nullFromSqlSubstitute: NullSubstitute<C> = getDefaultNullFromSqlSubstitute(false),
+            propertyExtractor: PropertyExtractor<T, C> = buildDefaultExtractorFrom(property)):
             Column<T, C> {
 
         return addColumn(
-                name, converter, nullFromSqlSubstitute, buildNullToSqlSubstitute(false),
+                name, converter, nullFromSqlSubstitute, getDefaultNullToSqlSubstitute(false),
                 propertyExtractor
         )
     }
 
 
     fun <C> addColumn(
-            name: String, converter: Converter<C>, nullFromSqlSubstitute: NullSubstitute<C>,
-            nullToSqlSubstitute: NullSubstitute<C>, propertyExtractor: PropertyExtractor<T, C>):
+            name: String,
+            converter: Converter<C>,
+            nullFromSqlSubstitute: NullSubstitute<C>,
+            nullToSqlSubstitute: NullSubstitute<C>,
+            propertyExtractor: PropertyExtractor<T, C>):
             Column<T, C> {
 
         val column = ColumnImpl(
-                name, propertyExtractor, converter, nullFromSqlSubstitute, nullToSqlSubstitute
+                name, propertyExtractor, converter,
+                nullFromSqlSubstitute, nullToSqlSubstitute
         )
 
         (allColumns as MutableSet).add(column)
@@ -67,60 +74,12 @@ abstract class BaseTable<T : Any, ID : Any>(
     }
 
 
-    fun <C> buildPropertyExtractorFrom(property: KProperty1<T, C>): PropertyExtractor<T, C> {
-        return SimplePropertyExtractor(property)
+    inline fun <reified C> getConverterForNullableType(): Converter<C> {
+        return configuration.getConverterForNullableType(getJavaClassFor(C::class))
     }
 
 
-
-    companion object {
-
-        val nullReturningNullSubstitute = object : NullSubstitute<Any?> {
-            override fun value() = null
-        }
-
-        val throwingNullFromSqlSubstitute = object : NullSubstitute<Any> {
-            override fun value() = throw NullPointerException(
-                    "Trying to assign return null for a non-null type property!!. Solution: " +
-                            "make db column non-null or provide a valid ${NullSubstitute::class}"
-            )
-        }
-
-        val throwingNullToSqlSubstitute = object : NullSubstitute<Any> {
-            override fun value() = throw NullPointerException(
-                    "Something is wrong!! Let the developer know"
-            )
-        }
-
-
-        fun <C> buildNullFromSqlSubstitute(isNullableProperty: Boolean): NullSubstitute<C> {
-            @Suppress("UNCHECKED_CAST")
-            return when (isNullableProperty) {
-                true -> nullReturningNullSubstitute
-                false -> throwingNullFromSqlSubstitute
-            } as NullSubstitute<C>
-        }
-
-
-        fun <C> buildNullToSqlSubstitute(isNullableProperty: Boolean): NullSubstitute<C> {
-            @Suppress("UNCHECKED_CAST")
-            return when (isNullableProperty) {
-                true -> nullReturningNullSubstitute
-                false -> throwingNullToSqlSubstitute
-            } as NullSubstitute<C>
-        }
-
-
-        /**
-         * This is used as a work around to get Class<> from a generic type that is upper
-         * bounded by Any?
-         *
-         * **CAUTION: Don't use this unless you perfectly know what this method does &
-         * what it is used for**
-         */
-        fun <C> getJavaClassFor(clazz: KClass<*>): Class<C> {
-            @Suppress("UNCHECKED_CAST")
-            return clazz.java as Class<C>
-        }
+    inline fun <reified C : Any> getConverterForNonNullType(): Converter<C> {
+        return configuration.getConverterForNonNullType(C::class.java)
     }
 }
