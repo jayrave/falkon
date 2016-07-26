95319ef5fc7274a086f9d6bcf8fdb4f3bf757133
jayrave <jayanthan.raveendiran@gmail.com>
2016-07-26 01:41:02 -0700

    make Column.qualifiedName & Column.getAppropriateName public

--- falkon-dao/src/main/kotlin/com/jayrave/falkon/dao/lib/ColumnExtn.kt
+++ falkon-dao/src/main/kotlin/com/jayrave/falkon/dao/lib/ColumnExtn.kt
@@ -5,14 +5,14 @@ import com.jayrave.falkon.mapper.Column
 /**
  * Gives the qualified name of a column. It is of the format "table_name.column_name"
  */
-internal val Column<*, *>.qualifiedName: String
+val Column<*, *>.qualifiedName: String
     get() = "${table.name}.$name"
 
 
 /**
  * @return [qualifiedName] or [Column.name] depending on [qualifyColumnName]
  */
-internal fun Column<*, *>.getAppropriateName(qualifyColumnName: Boolean): String {
+fun Column<*, *>.getAppropriateName(qualifyColumnName: Boolean): String {
     return when (qualifyColumnName) {
         true -> qualifiedName
         else -> name
