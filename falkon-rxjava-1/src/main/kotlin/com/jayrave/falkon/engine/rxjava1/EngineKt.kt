c1df40c75f24f9cb0b3abceb3c741f8471bbb246
jayrave <jayanthan.raveendiran@gmail.com>
2016-10-06 10:42:24 -0700

    add Engine#createCompiledQueryObservable extension function

--- falkon-rxjava-1/src/main/kotlin/com/jayrave/falkon/engine/rxjava1/EngineKt.kt
+++ falkon-rxjava-1/src/main/kotlin/com/jayrave/falkon/engine/rxjava1/EngineKt.kt
@@ -4,6 +4,42 @@ import com.jayrave.falkon.engine.*
 import rx.Observable
 import rx.Scheduler
 import rx.Subscriber
+import rx.functions.Func1
+
+/**
+ * Creates an [Observable] which will notify the subscribers with an appropriate
+ * [CompiledStatement] that represents the compiled query for passed in [rawSql] &
+ * [arguments], every time there is a [DbEvent] associated with the passed in
+ * [tableNames]
+ *
+ * *NOTE:*
+ *      - Subscribers are responsible for closing the [CompiledStatement]
+ *      - A statement is compiled on subscription
+ *      - *Unsubscribe* when done, to clean up
+ *
+ * @param tableNames names of tables this [rawSql] is concerned with
+ * @param rawSql raw SELECT statement
+ * @param arguments to be bound to [rawSql]
+ * @param scheduler to compile the query & deliver result on
+ *
+ * @return observable of compiled query for the given [rawSql] & [arguments]
+ */
+fun Engine.createCompiledQueryObservable(
+        tableNames: Iterable<String>, rawSql: String, arguments: Iterable<Any>?,
+        scheduler: Scheduler): Observable<CompiledStatement<Source>> {
+
+    return Observable.defer {
+        val predicate = Func1<Iterable<DbEvent>, Boolean> {
+            it.any { tableNames.contains(it.tableName) }
+        }
+
+        createDbEventObservable(scheduler)
+                .filter(predicate) // Only allow events for tables this query is concerned with
+                .startWith(emptyList<DbEvent>()) // To execute the query on subscription
+                .map { compileQuery(tableNames, rawSql).bindAll(arguments) }
+    }
+}
+
 
 /**
  * An [Observable] that notifies the subscribers about [DbEvent]s
@@ -59,4 +95,3 @@ private class DbEventForwardingOnSubscribe(private val engine: Engine) :
         }
     }
 }
-
---
+++ falkon-rxjava-1/src/test/kotlin/com/jayrave/falkon/engine/rxjava1/CreateCompiledQueryObservableTest.kt
@@ -0,0 +1,167 @@
+package com.jayrave.falkon.engine.rxjava1
+
+import com.jayrave.falkon.engine.*
+import com.nhaarman.mockito_kotlin.mock
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import rx.Observable
+import rx.schedulers.Schedulers
+import java.util.*
+import java.util.concurrent.CountDownLatch
+
+class CreateCompiledQueryObservableTest {
+
+    private val engineForTest = EngineForTest()
+
+    @Test
+    fun testCompiledQueryIsDeliveredOnSubscription() {
+        val countDownLatch = CountDownLatch(1)
+
+        engineForTest
+                .createDefaultObservableQuery()
+                .subscribe { countDownLatch.countDown() }
+
+        countDownLatch.awaitWithDefaultTimeout()
+        assertThat(engineForTest.compiledQueriesFor).hasSize(1)
+        assertThat(engineForTest.compiledQueriesFor.first()).isEqualTo(defaultQueryInfo)
+    }
+
+
+    @Test
+    fun testCompiledQueryIsDeliveredOnInsert() {
+        fireRelatedEventsAndAssertCompiledQueryDelivery(
+                engineForTest.createDefaultObservableQuery(), defaultQueryInfo,
+                DbEvent.forInsert(tableNameForDefaultQuery),
+                listOf(
+                        DbEvent.forInsert(tableNameForDefaultQuery),
+                        DbEvent.forInsert(tableNameNotInDefaultQuery)
+                )
+        )
+    }
+
+
+    @Test
+    fun testCompiledQueryIsDeliveredOnUpdate() {
+        fireRelatedEventsAndAssertCompiledQueryDelivery(
+                engineForTest.createDefaultObservableQuery(), defaultQueryInfo,
+                DbEvent.forUpdate(tableNameForDefaultQuery),
+                listOf(
+                        DbEvent.forUpdate(tableNameForDefaultQuery),
+                        DbEvent.forUpdate(tableNameNotInDefaultQuery)
+                )
+        )
+    }
+
+
+    @Test
+    fun testCompiledQueryIsDeliveredOnDelete() {
+        fireRelatedEventsAndAssertCompiledQueryDelivery(
+                engineForTest.createDefaultObservableQuery(), defaultQueryInfo,
+                DbEvent.forDelete(tableNameForDefaultQuery),
+                listOf(
+                        DbEvent.forDelete(tableNameForDefaultQuery),
+                        DbEvent.forDelete(tableNameNotInDefaultQuery)
+                )
+        )
+    }
+
+
+    /**
+     * Both [event] & [events] should deal with the tables [compiledQueryObservable]
+     * is related with
+     */
+    private fun fireRelatedEventsAndAssertCompiledQueryDelivery(
+            compiledQueryObservable: Observable<CompiledStatement<Source>>,
+            expectedQueryInfo: QueryInfo, event: DbEvent, events: List<DbEvent>) {
+
+        // Setup count down latches
+        val subscriptionCountDownLatch = CountDownLatch(1)
+        val eventCountDownLatch = CountDownLatch(1)
+        val eventListCountDownLatch = CountDownLatch(1)
+        val latchCountDownSerializer = LatchCountDownSerializer(
+                subscriptionCountDownLatch, eventCountDownLatch, eventListCountDownLatch
+        )
+
+        // Subscribe to the given observable
+        compiledQueryObservable.subscribe {
+            it.close()
+            latchCountDownSerializer.countDown()
+        }
+
+        // Wait for compiled query delivery on subscription
+        subscriptionCountDownLatch.awaitWithDefaultTimeout()
+        assertThat(engineForTest.compiledQueriesFor).hasSize(1)
+
+        // Fire event & make sure that compiled query is delivered
+        engineForTest.fireEvent(event)
+        eventCountDownLatch.awaitWithDefaultTimeout()
+        assertThat(engineForTest.compiledQueriesFor).hasSize(2)
+
+        // Fire event list & make sure that compiled query is delivered
+        engineForTest.fireEvents(events)
+        eventListCountDownLatch.awaitWithDefaultTimeout()
+        assertThat(engineForTest.compiledQueriesFor).hasSize(3)
+
+        // Assert that the expected compiled query was delivered every time
+        engineForTest.compiledQueriesFor.forEach { assertThat(it).isEqualTo(expectedQueryInfo) }
+    }
+
+
+
+    private class EngineForTest : Engine {
+
+        val compiledQueriesFor = ArrayList<QueryInfo>()
+        private val dbEventListeners = ArrayList<DbEventListener>()
+
+        override fun <R> executeInTransaction(operation: () -> R) = throw exception()
+        override fun isInTransaction() = throw exception()
+        override fun compileSql(tableNames: Iterable<String>?, rawSql: String) = throw exception()
+        override fun compileInsert(tableName: String, rawSql: String) = throw exception()
+        override fun compileUpdate(tableName: String, rawSql: String) = throw exception()
+        override fun compileDelete(tableName: String, rawSql: String) = throw exception()
+        private fun exception() = UnsupportedOperationException("not implemented")
+
+        override fun compileQuery(tableNames: Iterable<String>, rawSql: String):
+                CompiledStatement<Source> {
+
+            compiledQueriesFor.add(QueryInfo(tableNames, rawSql))
+            return mock()
+        }
+
+        override fun registerDbEventListener(dbEventListener: DbEventListener) {
+            dbEventListeners.add(dbEventListener)
+        }
+
+        override fun unregisterDbEventListener(dbEventListener: DbEventListener) {
+            dbEventListeners.remove(dbEventListener)
+        }
+
+        fun fireEvent(event: DbEvent) = dbEventListeners.forEach { it.onEvent(event) }
+        fun fireEvents(events: List<DbEvent>) = dbEventListeners.forEach { it.onEvents(events) }
+    }
+
+
+
+    private data class QueryInfo(val tableNames: Iterable<String>, val rawSql: String)
+
+
+
+    companion object {
+
+        private const val tableNameForDefaultQuery = "table in default query"
+        private const val tableNameNotInDefaultQuery = "table not in default query"
+        private const val rawSqlForDefaultQuery = "this is an example raw sql"
+        private val defaultQueryInfo = QueryInfo(
+                listOf(tableNameForDefaultQuery), rawSqlForDefaultQuery
+        )
+
+        private fun Engine.createDefaultObservableQuery(arguments: Iterable<Any>? = null):
+                Observable<CompiledStatement<Source>> {
+
+            return createCompiledQueryObservable(
+                    listOf(tableNameForDefaultQuery), rawSqlForDefaultQuery,
+                    arguments, Schedulers.newThread()
+            )
+        }
+    }
+}
