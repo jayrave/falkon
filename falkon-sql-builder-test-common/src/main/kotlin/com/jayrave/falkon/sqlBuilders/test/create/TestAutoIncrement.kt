bc09d5f6b165df30d8bc372d2dacf8fcf62ebd38
jayrave <jayanthan.raveendiran@gmail.com>
2016-11-09 00:37:58 -0800

    sql-builder-test-common: add helper for testing auto increment column

---
+++ falkon-sql-builder-test-common/src/main/kotlin/com/jayrave/falkon/sqlBuilders/test/create/TestAutoIncrement.kt
@@ -0,0 +1,52 @@
+package com.jayrave.falkon.sqlBuilders.test.create
+
+import com.jayrave.falkon.sqlBuilders.CreateTableSqlBuilder
+import com.jayrave.falkon.sqlBuilders.test.DbForTest
+import com.jayrave.falkon.sqlBuilders.test.buildArgListForSql
+import org.assertj.core.api.Assertions.assertThat
+
+class TestAutoIncrement(
+        private val createTableSqlBuilder: CreateTableSqlBuilder,
+        private val db: DbForTest) {
+
+    fun `auto incrementing column increments value by at least 1 if not explicitly inserted`() {
+
+        // Create table with auto incrementing primary key
+        val counterColumn = ColumnInfoForTest("counter", db.intDataType)
+        val idColumn = ColumnInfoForTest("id", db.intDataType, isId = true, autoIncrement = true)
+        val tableInfo = TableInfoForTest(
+                "test", listOf(idColumn, counterColumn), emptyList(), emptyList()
+        )
+
+        db.execute(createTableSqlBuilder.build(tableInfo))
+
+        // Insert records with increasing counter
+        val counter = 1..5
+        counter.forEach {
+            db.execute(
+                    "INSERT INTO ${tableInfo.name} (${counterColumn.name}) " +
+                            "VALUES (${buildArgListForSql(it)})"
+            )
+        }
+
+        // Get all records from db
+        val allRecords = db.findAllRecordsInTable(tableInfo.name)
+        assertThat(allRecords).hasSize(counter.count())
+
+        // Assert auto increment works
+        allRecords.take(allRecords.size - 1).forEachIndexed { index, currentRecord ->
+            // Extract fields of current record
+            val currentRecordId = currentRecord[idColumn.name]!!.toInt()
+            val currentRecordCounter = currentRecord[counterColumn.name]!!.toInt()
+
+            // Extract fields of next record
+            val nextRecord = allRecords[index + 1]
+            val nextRecordId = nextRecord[idColumn.name]!!.toInt()
+            val nextRecordCounter = nextRecord[counterColumn.name]!!.toInt()
+
+            assertThat(Math.signum(currentRecordId.compareTo(nextRecordId).toFloat())).isEqualTo(
+                    Math.signum(currentRecordCounter.compareTo(nextRecordCounter).toFloat())
+            )
+        }
+    }
+}
