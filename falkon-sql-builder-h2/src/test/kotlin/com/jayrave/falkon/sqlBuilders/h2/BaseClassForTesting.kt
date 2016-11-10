f965784e26255070648e81333d26694d3d2e45ee
jayrave <jayanthan.raveendiran@gmail.com>
2016-11-09 19:08:45 -0800

    sql-builder-h2: add tests for create table sql building

---
+++ falkon-sql-builder-h2/src/test/kotlin/com/jayrave/falkon/sqlBuilders/h2/BaseClassForTesting.kt
@@ -0,0 +1,35 @@
+package com.jayrave.falkon.sqlBuilders.h2
+
+import com.jayrave.falkon.sqlBuilders.test.DbForTest
+import com.jayrave.falkon.sqlBuilders.test.DbForTestViaJdbc
+import org.h2.jdbcx.JdbcConnectionPool
+import org.junit.After
+import org.junit.Before
+import javax.sql.DataSource
+
+/**
+ * Before every test, a clean database is setup
+ */
+abstract class BaseClassForTesting {
+
+    private lateinit var dataSource: DataSource
+    protected lateinit var db: DbForTest
+
+    @Before
+    fun setUp() {
+        // http://www.h2database.com/html/features.html#in_memory_databases
+        // Give the database a name to enabled multiple connections to the same database
+        dataSource = JdbcConnectionPool.create("jdbc:h2:mem:test;DB_CLOSE_DELAY=0;DATABASE_TO_UPPER=false", "user", "pw")
+        db = DbForTestViaJdbc(dataSource)
+    }
+
+
+    @After
+    fun tearDown() {
+        // http://www.h2database.com/html/grammar.html#shutdown
+        // http://www.h2database.com/html/features.html#in_memory_databases
+        // By default h2 closes the database when all existing connections to it are closed.
+        // This makes sure that we have a clean slate for every test
+        dataSource.connection.prepareStatement("SHUTDOWN").execute()
+    }
+}
---
+++ falkon-sql-builder-h2/src/test/kotlin/com/jayrave/falkon/sqlBuilders/h2/H2CreateTableSqlBuilderTest.kt
@@ -0,0 +1,59 @@
+package com.jayrave.falkon.sqlBuilders.h2
+
+import com.jayrave.falkon.sqlBuilders.test.create.*
+import org.junit.Test
+
+class H2CreateTableSqlBuilderTest : BaseClassForTesting() {
+
+    val createTableSqlBuilder = H2CreateTableSqlBuilder()
+
+    @Test
+    fun `simple primary key does not allow duplicates`() {
+        TestSimplePrimaryKey(createTableSqlBuilder, db).`simple primary key does not allow duplicates`()
+    }
+
+    @Test
+    fun `simple primary key cannot be null`() {
+        TestSimplePrimaryKey(createTableSqlBuilder, db).`simple primary key cannot be null`()
+    }
+
+    @Test
+    fun `composite primary key does not allow duplicates`() {
+        TestCompositePrimaryKey(createTableSqlBuilder, db).`composite primary key does not allow duplicates`()
+    }
+
+    @Test
+    fun `composite primary key does not allow any attributes to be null`() {
+        TestCompositePrimaryKey(createTableSqlBuilder, db).`composite primary key does not allow any attributes to be null`()
+    }
+
+    @Test
+    fun `max sized column does not allow values that are too big`() {
+        TestMaxSize(createTableSqlBuilder, db).`max sized column does not allow values that are too big`()
+    }
+
+    @Test
+    fun `non-null column does not allow null values`() {
+        TestNonNullability(createTableSqlBuilder, db).`non-null column does not allow null values`()
+    }
+
+    @Test
+    fun `auto incrementing column increments value by at least 1 if not explicitly inserted`() {
+        TestAutoIncrement(createTableSqlBuilder, db).`auto incrementing column increments value by at least 1 if not explicitly inserted`()
+    }
+
+    @Test
+    fun `column with uniqueness constraint does not allow duplicates`() {
+        TestSingleColumnUniquenessConstraint(createTableSqlBuilder, db).`column with uniqueness constraint does not allow duplicates`()
+    }
+
+    @Test
+    fun `multi column uniqueness constraint does not allow duplicates`() {
+        TestMultiColumnUniquenessConstraint(createTableSqlBuilder, db).`multi column uniqueness constraint does not allow duplicates`()
+    }
+
+    @Test
+    fun `foreign key constraint violating operation isn't allowed`() {
+        TestForeignKeyConstraint(createTableSqlBuilder, db).`foreign key constraint violating operation isn't allowed`()
+    }
+}
