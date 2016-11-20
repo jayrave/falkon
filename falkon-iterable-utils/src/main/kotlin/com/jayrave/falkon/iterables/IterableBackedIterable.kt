30aba45747bdec85f2d6981403c79c9e0186ee2d
jayrave <jayanthan.raveendiran@gmail.com>
2016-11-19 20:10:35 -0800

    falkon-iterable-utils: add IterableBackedIterable

---
+++ falkon-iterable-utils/src/main/kotlin/com/jayrave/falkon/iterables/IterableBackedIterable.kt
@@ -0,0 +1,20 @@
+package com.jayrave.falkon.iterables
+
+/**
+ * To expose items from an iterable in a different form
+ */
+internal class IterableBackedIterable<T, out R>(
+        private val iterable: Iterable<T>,
+        private val transformer: (T) -> R) : Iterable<R> {
+
+    override fun iterator(): Iterator<R> = IteratorBackedIterator()
+
+
+    private inner class IteratorBackedIterator : Iterator<R> {
+
+        private val iterator by lazy { iterable.iterator() }
+
+        override fun hasNext() = iterator.hasNext()
+        override fun next(): R = transformer.invoke(iterator.next())
+    }
+}
---
+++ falkon-iterable-utils/src/test/kotlin/com/jayrave/falkon/iterables/IterableBackedIterableTest.kt
@@ -0,0 +1,31 @@
+package com.jayrave.falkon.iterables
+
+import org.assertj.core.api.Assertions.*
+
+import org.junit.Test
+
+class IterableBackedIterableTest {
+
+    @Test
+    fun `for empty iterable`() {
+        assertThat(IterableBackedIterable(emptyList<Int>(), {})).isEmpty()
+    }
+
+
+    @Test
+    fun `for non empty iterable`() {
+        val intIterable = listOf(1, 2, 3, 4, 5)
+        val stringIterable = intIterable.map(Int::toString)
+        val intToStringIterable = IterableBackedIterable(intIterable, Int::toString)
+
+        val stringIterator = stringIterable.iterator()
+        val intToStringIterator = intToStringIterable.iterator()
+
+        while (stringIterator.hasNext()) {
+            assertThat(stringIterator.next()).isEqualTo(intToStringIterator.next())
+        }
+
+        assertThat(stringIterator.hasNext()).isFalse()
+        assertThat(intToStringIterator.hasNext()).isFalse()
+    }
+}
