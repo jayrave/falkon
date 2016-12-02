e52204b6963fce8260babb494840b31919883933
jayrave <jayanthan.raveendiran@gmail.com>
2016-12-01 21:06:20 -0800

    mapper: add `Realizer`

---
+++ falkon-mapper/src/main/kotlin/com/jayrave/falkon/mapper/Realizer.kt
@@ -0,0 +1,32 @@
+package com.jayrave.falkon.mapper
+
+/**
+ * Used to realize instances of classes that don't map exactly to a table. For eg., this could
+ * be used to realize instances of classes that carry
+ *
+ *  - columns from multiple tables
+ *  - subset of columns from a single table
+ *  - columns that include the result of running aggregate functions etc.
+ */
+interface Realizer<out T : Any> {
+
+    /**
+     * @return An instance of [T] realized with values extracted out of [Value]
+     */
+    fun realize(value: Value): T
+
+
+    interface Value {
+        /**
+         * Implementation should give back the value corresponding to the
+         * passed in [columnName]
+         */
+        infix fun <C> of(columnName: String): C
+
+        /**
+         * Implementation should give back the value corresponding to the
+         * passed in [column]
+         */
+        infix fun <C> of(column: Column<*, C>): C
+    }
+}
